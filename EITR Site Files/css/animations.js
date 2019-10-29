55f6ef34bc1d5c3c22380ea250c83cd82937e15d
Sydney Sauer <sydney.sauer@outlook.com>
2019-10-29 10:00:00 -0400

    Add files via upload
    First commit

---
+++ EITR Site Files/css/animations.js
@@ -0,0 +1,30 @@
+var i = 0;
+var images = [];
+var time = 2000;
+document.write("This is javascript");
+addImage();
+
+//Image List
+images[0] = 'img/IMG_0044.JPG';
+
+function addImage() {
+	document.member-pic.src = images[0];
+}
+
+
+// Sticky nav bar on scroll
+ /*
+window.onscroll = function() {myFunction()};
+var menu = getElementbyId("menu");
+var sticky = menu.offsetTop();
+
+function myFunction() {
+	if (window.pageYOffset >= sticky) {
+		menu.classList.add("sticky");
+	}
+	else {
+		menu.classList.remove("sticky");
+	}
+} 
+
+*/
